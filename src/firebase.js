a3243ecac0dd2c303869d719a60e4e209458e288
Transit-Lumber <Tristan-Bulmer@live.nmit.ac.nz>
2022-11-10 13:18:08 +1300

    Added and linked Firebase scripts

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCd9xWamQmzDXv_hjdaKuaosrX-UywWZVc",
+    authDomain: "omni-focus-9ca27.firebaseapp.com",
+    databaseURL: "https://omni-focus-9ca27-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "omni-focus-9ca27",
+    storageBucket: "omni-focus-9ca27.appspot.com",
+    messagingSenderId: "308899448240",
+    appId: "1:308899448240:web:18a6cc13bcc325a0544275",
+    measurementId: "G-F7METZXDYJ"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const auth = firebase.auth();
