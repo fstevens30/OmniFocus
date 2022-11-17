1d772d2186dbe25c5734a6bc385b9470055f47b3
Transit-Lumber <Tristan-Bulmer@live.nmit.ac.nz>
2022-11-17 15:23:50 +1300

    Progress bar changes color depending on many are complete

--- src/dashboard/dash.js
+++ src/dashboard/dash.js
@@ -41,6 +41,8 @@ function start() {
             // Run the addTask function to add the task to the dashboard page
             addTask(doc.id, doc.data());
         });
+        // Update the progress bar
+        updateProgress();
     }).catch(function(error) {
         console.log("Error getting documents: ", error);
     }
@@ -56,6 +58,9 @@ function trash(id) {
     }).catch(function(error) {
         console.error("Error removing document: ", error);
     });
+
+    // Update the progress bar
+    updateProgress();
 }
 
 function newTask() {
@@ -71,6 +76,9 @@ function newTask() {
     db.collection("tasks").add(task).then(function(docRef) {
         // Success, add the task to the dashboard page
         addTask(docRef.id, task);
+
+        // Update the progress bar
+        updateProgress();
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
@@ -213,7 +221,9 @@ function updateTask(id) {
 function toggleActive(id) {
     // Toggle the active state of the task in the database
     db.collection("tasks").doc(id).update({
-        active: !document.getElementById(id + "-checkbox").checked
+        active: !document.getElementById(id + "-checkbox").checked,
+        lastMod: new Date()
+
     }).then(function() {
         console.log("Document successfully updated!");
     }).catch(function(error) {
@@ -223,4 +233,100 @@ function toggleActive(id) {
     // Toggle the active state of the task on the dashboard
     document.getElementById(id).classList.toggle("active");
     document.getElementById(id).classList.toggle("complete");
+
+    // Update the task date
+    document.getElementById(id + "-date").innerHTML = "Last modified just now";
+
+    // Update the progress bar
+    updateProgress();
+}
+
+function updateProgress() {
+    // Get the number of all tasks
+    var all = document.getElementsByClassName("task").length;
+
+    // Get the number of complete tasks
+    var complete = document.getElementsByClassName("complete").length;
+
+    // Update the ratio of complete tasks to all tasks
+    document.getElementById("progress-ratio").innerHTML = complete + "/" + all;
+
+    // Calculate the progress
+    var progress = (complete / all) * 100;
+
+    // Update the progress bar width
+    document.getElementsByClassName("progress-bar-fill")[0].style.width = progress + "%";
+
+    // Update the progress bar background color
+    document.getElementsByClassName("progress-bar-fill")[0].style.backgroundColor = progressColor(progress / 100);
+}
+
+function progressColor(decimal) {
+
+    console.log(decimal);
+    // Colors will be mixed depending on the percent of completion
+    // 1 (100%) = green (#008A0E)
+    // 0.8 (80%) = lime (#7EAC11)
+    // 0.6 (60%) = yellow (#FCCE14)
+    // 0.4 (40%) = orange (#CC4E00)
+    // 0.2 (20%) = red (#E81313)
+    // 0 (0%) = red (#E81313) but 0% opacity
+
+    // E.G progressColor(1) = #008A0E (100% green)
+    // E.G progressColor(0.9) =    #3f9b10 (50% green + 50% lime)
+    // E.G progressColor(0.8) = #7EAC11 (100% lime)
+
+    var ceilColor = "" // The first color to be mixed
+    var ratio = 0 // How much of the first color to mix with the second color (Second will be 1 - ratio)
+    var floorColor = "" // The second color to be mixed
+
+    if (decimal == 1) { // If the progress is 100% (all tasks are complete)
+        ceilColor = "#008A0E";
+        floorColor = "#008A0E";
+        ratio = 0.5; // Will mix black with itself, so it will just be black
+    }
+    else if (decimal >= 0.8) { // If the progress is between 80% and 100%
+        ceilColor = "#008A0E";
+        floorColor = "#7EAC11";
+        ratio = (decimal - 0.8) / 0.2; // Will mix so that the color transitions from green to lime as the progress goes from 100% to 80%
+    }
+    else if (decimal >= 0.6) { // If the progress is between 60% and 80%
+        ceilColor = "#7EAC11";
+        floorColor = "#FCCE14";
+        ratio = (decimal - 0.6) / 0.2; // Will mix so that the color transitions from lime to yellow as the progress goes from 80% to 60%
+    }
+    else if (decimal >= 0.4) {
+        ceilColor = "#FCCE14";
+        floorColor = "#CC4E00";
+        ratio = (decimal - 0.4) / 0.2; // Will mix so that the color transitions from yellow to orange as the progress goes from 60% to 40%
+    }
+    else if (decimal >= 0.2) { // If the progress is between 20% and 40%
+        ceilColor = "#CC4E00";
+        floorColor = "#E81313";
+        ratio = (decimal - 0.2) / 0.2; // Will mix so that the color transitions from orange to red as the progress goes from 40% to 20%
+    }
+    else if (decimal >= 0) { // If the progress is between 0% and 20%
+        color = "#E81313";
+        opacity = decimal / 0.2; // Will make the color more transparent as the progress goes from 20% to 0%
+        return "rgba(232, 19, 19, " + opacity + ")"; // Return the color with the opacity
+    }
+    else { // If the progress is 0% (no tasks are complete)
+        return "transparent"; // Will be transparent
+    }
+
+    // Mix the colors using the ratio provided
+
+    // Converts each hex value to an integer, adds them together using the ratio
+    var red = parseInt(ceilColor.substring(1, 3), 16) * ratio + parseInt(floorColor.substring(1, 3), 16) * (1 - ratio);
+    var green = parseInt(ceilColor.substring(3, 5), 16) * ratio + parseInt(floorColor.substring(3, 5), 16) * (1 - ratio);
+    var blue = parseInt(ceilColor.substring(5, 7), 16) * ratio + parseInt(floorColor.substring(5, 7), 16) * (1 - ratio);
+
+    // Converts the integers back to hex values (capatilized)
+    red = Math.round(red).toString(16).toUpperCase();
+    green = Math.round(green).toString(16).toUpperCase();
+    blue = Math.round(blue).toString(16).toUpperCase();
+
+    // Fixes the hex values that are only one character long (e.g. 0 instead of 00 or 9 instead of 09)
+    // Adds the RGB values together with the # in front to make a hex color
+    return "#" + (red.length == 1 ? "0" + red : red) + (green.length == 1 ? "0" + green : green) + (blue.length == 1 ? "0" + blue : blue);
 }
