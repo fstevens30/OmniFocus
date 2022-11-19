214266e968561d2017d71518bf41128dc13c4982
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-19 18:04:36 +1300

    Added more hosting stuff

--- index.js
+++
@@ -1,7 +0,0 @@
-// This runs when the app starts
-
-// Import the express module
-const express = require('express');
-
-// Create a new express app
-const app = express(window.open('https://www.google.com'));
