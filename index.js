cb896539778c795e8405d27bdf9d9333547bf1cc
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-19 17:55:00 +1300

    test

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // This runs when the app starts
 
-// Display the src/home/home.html file in the browser
+// Import the express module
+const express = require('express');
+
+// Create a new express app
+const app = express(window.open('https://www.google.com'));
