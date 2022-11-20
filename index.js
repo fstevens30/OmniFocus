d214cbacaba4dff44bc1099455a061bd88e0bc25
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-21 11:48:01 +1300

    changed the res.end() parameters

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000; // process.env.PORT is used by Heroku
 
 const server = http.createServer((req, res) => { // Create a server
     res.statusCode = 200; // OK
-    res.setHeader('Content-Type', 'text/plain'); // Set the content type
-    res.end('Hello World!'); // Send the response
+    res.setHeader('Content-Type', 'text/html'); // Set the content type
+    res.end('<h1>Hello World!</h1>'); // Send the response
 });
 
 server.listen(port, hostname, () => { // Listen on port 3000
