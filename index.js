d7905ef32789640812251f1ad73d439efc7ee37b
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-21 11:52:06 +1300

    Imported fs module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http'); // Import the http module
+const fs = require('fs'); // Import the fs module
 
 const hostname = '0.0.0.0'; // Define the hostname
 const port = process.env.PORT || 3000; // process.env.PORT is used by Heroku
@@ -6,7 +7,8 @@ const port = process.env.PORT || 3000; // process.env.PORT is used by Heroku
 const server = http.createServer((req, res) => { // Create a server
     res.statusCode = 200; // OK
     res.setHeader('Content-Type', 'text/html'); // Set the content type
-    res.end('<h1>Hello World!</h1>'); // Send the response
+    // Send the response as the index.html file
+    res.end(fs.readFileSync(__dirname + '/index.html', 'utf8'));
 });
 
 server.listen(port, hostname, () => { // Listen on port 3000
