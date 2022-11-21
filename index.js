93bb679e70542c27b8e9fe9fc38c999e16b4eb9b
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-21 15:08:52 +1300

    Added comment

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const hostname = '0.0.0.0'; // Define the hostname
 const port = process.env.PORT || 3000; // process.env.PORT is used by Heroku
 
 const server = http.createServer((req, res) => { // Create a server
-    res.statusCode = 200; // OK
-    res.setHeader('Content-Type', 'text/html'); // Set the content type
-    // Send the response as the index.html file
-    res.send(fs.readFileSync(__dirname + '/src/home/home.html', 'utf8'));
-    // I still need to link the css file
-    // I can do this by using the fs module to read the file and send it as a response
-    res.send(fs.readFileSync(__dirname + '/src/home/home.css', 'utf8'));
-    res.end(); // End the response
+    // Do something 
 });
 
 server.listen(port, hostname, () => { // Listen on port 3000
