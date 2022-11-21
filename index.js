18cffcda5143be2fd775bb26c083e3d0a4f96f44
Tristan Bulmer <63182722+Transit-Lumber@users.noreply.github.com>
2022-11-21 17:10:42 +1300

    Root directs to homepage

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const hostname = '0.0.0.0'; // Define the hostname
 const port = process.env.PORT || 3000; // process.env.PORT is used by Heroku
 
 const server = http.createServer((req, res) => { // Create a server
+    // Send the index file if no file is requested
+    if (req.url == '/') {
+        req.url = '/src/home/home.html";
+    }
+    
     // Send the requested file if it exists
     var url = __dirname + req.url
 
