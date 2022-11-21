6abacf3b5d537a0dbb216217c3adeb3308eea223
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-11-21 14:57:32 +1300

    Changed file method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => { // Create a server
     res.statusCode = 200; // OK
     res.setHeader('Content-Type', 'text/html'); // Set the content type
     // Send the response as the index.html file
-    res.send(fs.readFileSync(__dirname + '/src/home/home.html', 'utf8')); // Links to the index.html file
+    res.sendFile(__dirname + '/src/home/home.html');
     // I still need to link the css file
     // I can do this by using the fs module to read the file and send it as a response
     res.sendFile(__dirname + '/src/home/home.css'); // Links to the index.css file
